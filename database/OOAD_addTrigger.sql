c749cb4bb87ad8cdf94e53b37d611c7dc348ec09
YidanSheng <yidan@auctionsoftware.com>
2018-10-28 00:43:59 -0500

    trigger sql

--- database/OOAD_addTrigger.sql
+++ database/OOAD_addTrigger.sql
@@ -1,29 +1,5 @@
-DROP TRIGGER IF EXISTS check_new_comment;
-create trigger check_new_comment
-  after INSERT
-  on comment
-  for each row
-  begin
-    insert ignore into notification (course_id, user_id)
-    SELECT course_id, user_id
-    from subscribe
-    where course_id = NEW.course_id;
-  end;
-  
-DROP TRIGGER IF EXISTS delete_notification;
- create trigger delete_notification
-  after DELETE
-  on subscribe
-  for each row
-  begin
-    delete
-    from notification
-    where OLD.user_id = notification.user_id
-      and OLD.course_id = notification.course_id;
-  end;
-  
-  
 DELIMITER //
+DROP TRIGGER IF EXISTS check_new_comment;
 create trigger check_new_comment
   after INSERT
   on comment
@@ -37,8 +13,8 @@ create trigger check_new_comment
   
 DELIMITER //
 
-  
- create trigger delete_notification
+DROP TRIGGER IF EXISTS delete_notification;
+create trigger delete_notification
   after DELETE
   on subscribe
   for each row
@@ -47,6 +23,6 @@ DELIMITER //
     from notification
     where OLD.user_id = notification.user_id
       and OLD.course_id = notification.course_id;
-   end//
+  end//
   
   
